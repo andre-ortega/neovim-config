502ce745371d1ec5debf902f4a93f40de30229b7
andre-ortega <ortega.andre.j@gmail.com>
2024-06-29 23:26:53 -0700

    vsnip troubles

--- lua/punkmonk/init.lua
+++ lua/punkmonk/init.lua
@@ -1,7 +1,7 @@
 require("punkmonk.lazy")
 require("punkmonk.remap")
 require("punkmonk.set")
+require("punkmonk.treesitter")
 require("punkmonk.telescope")
 require("punkmonk.lsp_config")
 require("punkmonk.cmp")
-require("punkmonk.treesitter")
